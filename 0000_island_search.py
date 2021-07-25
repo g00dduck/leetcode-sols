93faf3caabe4085d49957207f35fac320986e3a2
Shinni <sshinn09@gmail.com>
2021-07-24 20:15:28 -0500

    Sorted out confusion over array grid versus traditional coordinate.
    Added another check to not stop searching if we're on the starting coordinate. Feels hacky; may not be correct solution?

--- 0000_island_search.py
+++ 0000_island_search.py
@@ -25,9 +25,9 @@ from typing import List
 class Solution:
     def pretty_print_map(self, grid: List[List[str]]) -> str:
         pretty_map = ""
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                pretty_map = pretty_map + f"\t{grid[x][y]}"
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                pretty_map = pretty_map + f"\t{grid[y][x]}"
             pretty_map += "\n"
 
         return pretty_map
@@ -39,15 +39,15 @@ class Solution:
         visited_coords = [[0 for _y in range(len(grid[0]))] for _x in range(len(grid))]
 
         # Start map traversal!
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
                 # If this is water or land we've already visited, we're not 
                 # intereseted; move to next coord.
-                if int(grid[x][y]) == 0:
-                    print("This is water! Continuing traversal.") 
+                if int(grid[y][x]) == 0:
+                    print(f"This is water ({x}, {y})! Continuing traversal.") 
                     continue
-                if visited_coords[x][y] == 1:
-                    print("We've been here! Continuing traversal.")
+                if visited_coords[y][x] == 1:
+                    print(f"We've been here ({x}, {y})! Continuing traversal.")
                     continue
 
                 # This is a piece of unvisited land! Start an island search.
@@ -60,25 +60,23 @@ class Solution:
                 search_y = y
                 search_direction = "N"
                 while searching_island:
-                    visited_coords[search_x][search_y] = 1
-                    print(f"Searching island!! On coord ({search_x}, {search_y}) going {search_direction}. "
-                        f"Visited land map:\n{self.pretty_print_map(visited_coords)}")
+                    visited_coords[search_y][search_x] = 1
+                    print(f"Searching island!! On coord ({search_x}, {search_y}) "
+                        f"going {search_direction}.")
 
                     # Check for borders/water first, then visited land. When found,
                     # either quit searching or change direction. If neither are 
                     # found, continue searching in the same direction.
                     if search_direction == "N":
                         search_y = search_y - 1
-                        if search_y == -1:
-                            print("** On a border cell; resetting coord and changing direction.")
+                        on_starting_coord = search_x == x and search_y == y
+                        if search_y == -1 or int(grid[search_y][search_x]) == 0:
+                            print("** There's water to the north of here!! resetting coord"
+                                f" ({search_x}, {search_y}) and changing direction.")
+                            search_y = search_y + 1
                             water_N = True
                             search_direction = "E"
-                            search_y = 0
-                        elif int(grid[search_x][search_y]) == 0:
-                            print("** There's water to the north of here!! Changing direction.")
-                            water_N = True
-                            search_direction = "E"
-                        elif visited_coords[search_x][search_y] == 1:
+                        elif not on_starting_coord and visited_coords[search_y][search_x] == 1:
                             print("** We've been here before! Quit searching.")
                             searching_island = False
                         else:
@@ -86,41 +84,55 @@ class Solution:
                             search_direction = "N"
                     elif search_direction == "E":
                         search_x = search_x + 1
-                        if search_x == len(grid) or int(grid[search_x][search_y]) == 0:
+                        on_starting_coord = search_x == x and search_y == y
+                        if search_x == len(grid[0]) or int(grid[search_y][search_x]) == 0:
+                            print("** There's water to the east of here!! resetting coord "
+                                f"({search_x}, {search_y}) and changing direction.")
                             water_E = True
                             search_direction = "S"
-                            if search_x == len(grid):
-                                search_x = len(grid) - 1
-                        elif visited_coords[search_x][search_y] == 1:
+                            search_x = search_x - 1
+                        elif not on_starting_coord and visited_coords[search_y][search_x] == 1:
+                            print("** We've been here before! Quit searching.")
                             searching_island = False
                         else:
+                            print("** Continuing east!")
                             search_direction = "E"
                     elif search_direction == "S":
                         search_y = search_y + 1
-                        if search_y == len(grid[0]) or int(grid[search_x][search_y]) == 0:
+                        on_starting_coord = search_x == x and search_y == y
+                        if search_y == len(grid) or int(grid[search_y][search_x]) == 0:
+                            print("** There's water to the south of here!! Resetting coord "
+                                f"({search_x}, {search_y}) and changing direction.")
                             water_S = True
                             search_direction = "W"
-                            if search_y == len(grid[0]):
-                                search_y = len(grid[0]) - 1
-                        elif visited_coords[search_x][search_y] == 1:
+                            search_y = search_y - 1
+                        elif not on_starting_coord and visited_coords[search_y][search_x] == 1:
+                            print("** We've been here before! Quit searching.")
                             searching_island = False
                         else:
+                            print("** Continuing south!")
                             search_direction = "S"
                     elif search_direction == "W":
                         search_x = search_x - 1
-                        if search_x == -1 or int(grid[search_x][search_y]) == 0:
+                        on_starting_coord = search_x == x and search_y == y
+                        if search_x == -1 or int(grid[search_y][search_x]) == 0:
+                            print("** There's water to the west of here!! Resetting coord ("
+                                f"{search_x}, {search_y}) and changing direction.")
                             water_W = True
                             search_direction = "N"
-                            if search_x == -1:
-                                search_x = 0
-                        elif visited_coords[search_x][search_y] == 1:
+                            search_x = search_x + 1
+                        elif not on_starting_coord and visited_coords[search_y][search_x] == 1:
+                            print("** We've been here before! Quit searching.")
                             searching_island = False
                         else:
+                            print("** Continuing west!")
                             search_direction = "W"
 
                     if water_N and water_E and water_S and water_W:
                         num_islands += 1
                         searching_island = False
+
+                    print(f"Visited land map:\n{self.pretty_print_map(visited_coords)}")
                 print("\nFinished searching island! Continuing traversal.")
 
         return num_islands
@@ -129,12 +141,12 @@ def main():
     #while True:
         #try:
             island_map = [
+                ["1","1","1","1","0"],
+                ["1","1","0","1","0"],
                 ["1","1","0","0","0"],
-                ["1","1","0","0","0"],
-                ["0","0","1","0","0"],
-                ["0","0","0","1","1"]
-            ]            
-
+                ["0","0","0","0","0"]
+            ]
+            
             print(f"Here's the island map we're working with:\n{Solution().pretty_print_map(island_map)}")
             print(f"\nThis map contains {Solution().get_num_islands(island_map)} islands!")
 
